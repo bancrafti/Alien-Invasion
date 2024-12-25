54552c0d2259001c3d0644f299de014b00c72d02
bancrafti <swishieway@gmail.com>
2024-12-25 13:35:23 +0300

    other minor changes

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys  # exit
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -18,14 +19,24 @@ def run_game():
     # Make a ship
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets in
+    bullets = Group()
+
     # set background color
     bg_color = (230, 230, 230)
 
     # Start the loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+
+        # Get rid pf bullets that have disappeared
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+            print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
